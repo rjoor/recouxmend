576afa0c20b16ec2fea176896faab76f40bba659
Ross Jaeger <rjaeger@adaptavist.com>
2022-12-03 09:58:04 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: ['.ts', '.tsx', '.js', '.json'],
+                },
+                use: 'ts-loader',
+            },
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', ['@babel/preset-react', { "runtime": "automatic" }]]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        fallback: {
+            "fs": false,
+            "os": false,
+            "path": false
+        }
+    }
+
+}
