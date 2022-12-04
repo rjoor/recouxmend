c6d7d2bb89480449781aa557bb78af2c71793115
Ross Jaeger <rjaeger@adaptavist.com>
2022-12-03 22:43:31 -0500

    Started work on auth

--- client/src/components/ApiTest.tsx
+++ client/src/components/ApiTest.tsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 
 const ApiTest = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<{ message: string }>({ message: "" });
 
   useEffect(() => {
     fetch("http://localhost:3001/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
-    console.log(data);
+      .then((data) => setData({ message: data.message }));
   }, []);
 
-  return <>{data ? data : <h1>Waiting</h1>}</>;
+  return <>{data.message === "" ? <h1>Loading...</h1> : <h1>Success</h1>}</>;
 };
 
 export default ApiTest;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+require('dotenv').config()
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+const authOptions = {
+    method: 'POST',
+    body: `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+}
+
+fetch(process.env.AUTH_URL, authOptions)
+    .then((res) => res.json())
+    .then(data => console.log(data.access_token))
+
+app.post("/spotify", (req, res) => {
+
+})
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Success" });
+    res.json({
+        message: "Success"
+    });
     console.log(req.route.path)
 })
 
