b3acc6bd8875b29da701b57d73ca55b3583639b6
Ross Jaeger <rjaeger@adaptavist.com>
2022-12-09 18:56:41 -0500

    It works!

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { emitWarning } from "process";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [userInfoQueried, setUserInfoQueried] = useState<boolean>(false);
   const [search] = useSearchParams();
   const code = search.get("code")!;
   const state = search.get("state")!;
@@ -11,12 +13,30 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:3001/callback?" + new URLSearchParams(params)).then(
-      (res) => console.log(res)
-    );
-  }, []);
+    if (!userInfoQueried) {
+      fetch(
+        "http://localhost:3001/callback?" + new URLSearchParams(params)
+      ).then((res) => console.log(res));
+    } else {
+      fetch("http://localhost:3001/me")
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
+  }, [userInfoQueried]);
 
-  return <div>Welcome</div>;
+  const getUserInfoHandler = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setUserInfoQueried(true);
+  };
+
+  return (
+    <>
+      <div>Welcome</div>
+      <button onClick={getUserInfoHandler}>Get user info</button>
+    </>
+  );
 };
 
 export default Home;
--- server/index.js
+++ server/index.js
@@ -1,175 +1,75 @@
-const express = require("express");
-require('dotenv').config();
-const request = require('request');
-const cors = require('cors');
-const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
-
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET
-const redirect_uri = process.env.REDIRECT_URI
-
+const BASE = 'http://localhost:'
 const PORT = process.env.port || 3001;
+const express = require('express');
+require('dotenv').config();
 const app = express();
+const scopes = ['user-read-private', 'user-read-email'];
+const SpotifyWebApi = require("spotify-web-api-node");
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
+const credentials = {
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+}
+
+const spotifyApi = new SpotifyWebApi(credentials)
+
 const generateRandomString = function (length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (var i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
 };
 
-var stateKey = 'spotify_auth_state';
+const state = generateRandomString(16)
+console.log(state);
 
-app.use(cors())
-    .use(cookieParser());
+const authUrl = spotifyApi.createAuthorizeURL(scopes, state);
+console.log(authUrl)
 
-app.get('/login', function (req, res) {
+app.get('/login', (_, res) => {
+    res.redirect(authUrl);
+})
 
-    var state = generateRandomString(16);
-    res.cookie(stateKey, state);
+app.get('/callback*', (req, _) => {
+    const code = req.query.code
+    console.log(code);
+    spotifyApi.authorizationCodeGrant(code)
+        .then((data) => {
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.setRefreshToken(data.body['refresh_token']);
+            console.log('success!!')
+        })
+        .catch((err) => {
+            console.log('Oh no, something has gone horribly wrong! Namely...', err)
+        })
+})
 
-    // your application requests authorization
-    var scope = 'user-read-private user-read-email';
-    res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-            response_type: 'code',
-            client_id: client_id,
-            scope: scope,
-            redirect_uri: redirect_uri,
-            state: state
-        }));
-});
+app.get('/refresh', () => {
+    spotifyApi.refreshAccessToken()
+        .then((data) => {
+            console.log('Access token refreshed! Get frickin pwned old access token!')
+            spotifyApi.setAccessToken(data.body['access_token']);
+        })
+        .catch((err) => {
+            console.log('Nice try but the old access token isn\'t ready to say bye yet!', err)
+        })
+})
 
-app.get('/callback*', function (req, res) {
-
-    // your application requests refresh and access tokens
-    // after checking the state parameter
-
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-    var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-    if (state === null || state !== storedState) {
-        res.redirect('/#' +
-            querystring.stringify({
-                error: 'state_mismatch'
-            }));
-    } else {
-        res.clearCookie(stateKey);
-        var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirect_uri,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
-            },
-            json: true
-        };
-
-        request.post(authOptions, function (error, response, body) {
-            console.log("test");
-            if (!error && response.statusCode === 200) {
-
-                var access_token = body.access_token,
-                    refresh_token = body.refresh_token;
-
-                var options = {
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: { 'Authorization': 'Bearer ' + access_token },
-                    json: true
-                };
-
-                // use the access token to access the Spotify Web API
-                request.get(options, function (error, response, body) {
-                    console.log(body);
-                });
-
-                // we can also pass the token to the browser to make requests from there
-                res.redirect('/#' +
-                    querystring.stringify({
-                        access_token: access_token,
-                        refresh_token: refresh_token
-                    }));
-            } else {
-                res.redirect('/#' +
-                    querystring.stringify({
-                        error: 'invalid_token'
-                    }));
-            }
-        });
-    }
-});
-
-app.get('/refresh_token', function (req, res) {
-
-    // requesting access token from refresh token
-    var refresh_token = req.query.refresh_token;
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-        form: {
-            grant_type: 'refresh_token',
-            refresh_token: refresh_token
-        },
-        json: true
-    };
-
-    request.post(authOptions, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
-            res.send({
-                'access_token': access_token
-            });
-        }
-    });
-});
-
-app.get('/me')
-
-// const authOptions = {
-//     method: 'POST',
-//     body: `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//     }
-// }
-
-// const spotifyOptions = {
-//     url: ''
-// }
-
-// const testCall = (access_token) => {
-
-// }
-
-// fetch(process.env.AUTH_URL, authOptions)
-//     .then((res) => res.json())
-//     .then(data => testCall(data.access_token))
-
-// app.post("/spotify", (req, res) => {
-
-// })
-
-// app.get("/api", (req, res) => {
-//     res.json({
-//         message: "Success"
-//     });
-//     console.log(req.route.path)
-// })
+app.get('/me', (_, res) => {
+    console.log("getting info about myself!")
+    spotifyApi.getUser('rjoor')
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            console.log('Could not get user info :(')
+        })
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
-
